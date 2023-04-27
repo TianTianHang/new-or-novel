757e8a4e6e6d4f6e9f9e06c1a63dcb1bce1dd0c1
tiantian <a2450804878@hotmail.com>
2023-04-27 19:07:59 +0800

    fix api code bug

--- src/googleTrends/services.py
+++ src/googleTrends/services.py
@@ -1,6 +1,8 @@
 import pandas as pd
-
+from googleTrends.API.GoogleTrendsAPI import GoogleTrendsAPI
 import gevent
+
+API = GoogleTrendsAPI
 
 
 # geoName lat lon geoCode time words ......
@@ -9,32 +11,33 @@ def getDataByRegionAndOvertime(kw_list: list, timeframe_list: list):
     for timeframe in timeframe_list:
         heatValue = []
         for kw in kw_list:
-            heatValue.append(GoogleTrendsAPI.getDataByRegion(kw, timeframe))
+            heatValue.append(API.getDataByRegion(kw, timeframe))
         var: pd.DataFrame = pd.concat(heatValue, axis=1).T.drop_duplicates().T
         var.insert(1, 'time', timeframe)
         result.append(var)
     df = pd.concat(result)
-    return GoogleTrendsAPI.addGeo(df)
+    return API.addGeo(df)
 
 
 def getDataByRegionAndOvertimeGevent(kw_list: list, timeframe_list: list):
     result = []
-    greenlets = [gevent.spawn(GoogleTrendsAPI.getDataByRegion, kw, timeframe) for timeframe in timeframe_list for kw in kw_list]
+    greenlets = [gevent.spawn(API.getDataByRegion, kw, timeframe) for timeframe in timeframe_list for kw in
+                 kw_list]
     # wait for all greenlets to finish and get their results
     heatValue = [g.value for g in gevent.joinall(greenlets)]
     for i, timeframe in enumerate(timeframe_list):
         var = heatValue[i * len(kw_list)]
-        for j in range(i * len(kw_list)+1, i * len(kw_list) + len(kw_list)):
-            var = var.merge(heatValue[j], on=['geoName', 'geoCode'],)
+        for j in range(i * len(kw_list) + 1, i * len(kw_list) + len(kw_list)):
+            var = var.merge(heatValue[j], on=['geoName', 'geoCode'], )
         var.insert(1, 'time', timeframe)
         result.append(var)
     df = pd.concat(result)
-    return GoogleTrendsAPI.addGeo(df)
+    return API.addGeo(df)
 
 
 # time words ......
 def getDataOvertimeMultiWord(kw_list: list, timeframeOrList: str | list):
-    getDataFun = GoogleTrendsAPI.getDataOvertime
+    getDataFun = API.getDataOvertime
     # if isinstance(timeframeOrList, list):
     #     getDataFun = GoogleTrendsAPI.getDataOvertimeMultirange
     result = []
